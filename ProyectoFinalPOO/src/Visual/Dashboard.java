db17866bfb4fcfb32dd6c4bbcff1c9cc6620e118
jismah <ji_rq019@hotmail.com>
2021-07-21 14:18:02 -0400

    Actualizacion de Visual | Login, Dashboard

--- ProyectoFinalPOO/src/Visual/Dashboard.java
+++ ProyectoFinalPOO/src/Visual/Dashboard.java
@@ -1,15 +1,18 @@
 package Visual;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
 
 public class Dashboard extends JFrame {
 
 	private JPanel contentPane;
+	private Dimension dim;
 
 	/**
 	 * Launch the application.
@@ -33,7 +36,11 @@ public class Dashboard extends JFrame {
 	public Dashboard() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		dim = getToolkit().getScreenSize();
+		setSize(dim.width-200, dim.height-200);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
--- ProyectoFinalPOO/src/Visual/Login.java
+++ ProyectoFinalPOO/src/Visual/Login.java
@@ -1,15 +1,38 @@
 package Visual;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Cursor;
+import javax.swing.border.LineBorder;
+import java.awt.SystemColor;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.Component;
+import javax.swing.UIManager;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseMotionAdapter;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
+	private Dimension dim;
+	private JTextField txtUsuario;
+	private JPasswordField txtPassword;
+	int posX = 0, posY = 0;
 
 	/**
 	 * Launch the application.
@@ -19,6 +42,7 @@ public class Login extends JFrame {
 			public void run() {
 				try {
 					Login frame = new Login();
+					frame.setUndecorated(true);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -31,12 +55,117 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
+		setTitle("Login");
+		setBackground(Color.WHITE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		dim = getToolkit().getScreenSize();
+		setSize(dim.width - 1000, dim.height - 500);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new LineBorder(SystemColor.menu));
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
 
+		JPanel panel = new JPanel();
+
+		panel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				posX = e.getX();
+				posY = e.getY();
+			}
+		});
+
+		panel.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				setLocation (e.getXOnScreen()-posX,e.getYOnScreen()-posY);
+			}
+		});
+
+		panel.setBackground(Color.WHITE);
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+
+		JLabel lblNewLabel = new JLabel("TechShop");
+		lblNewLabel.setForeground(new Color(102, 102, 255));
+		lblNewLabel.setFont(new Font("Yu Gothic Medium", Font.PLAIN, 21));
+		lblNewLabel.setBounds(20, 13, 107, 36);
+		panel.add(lblNewLabel);
+
+		JLabel label = new JLabel("|");
+		label.setForeground(new Color(0, 0, 0));
+		label.setFont(new Font("Yu Gothic UI Light", Font.BOLD, 21));
+		label.setBounds(124, 11, 19, 36);
+		panel.add(label);
+
+		JLabel lblSistemaDeVentas = new JLabel("Sistema de Ventas");
+		lblSistemaDeVentas.setForeground(Color.BLACK);
+		lblSistemaDeVentas.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 18));
+		lblSistemaDeVentas.setBounds(137, 13, 152, 35);
+		panel.add(lblSistemaDeVentas);
+
+		JLabel lblCloseLogin = new JLabel("X");
+		lblCloseLogin.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		lblCloseLogin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		lblCloseLogin.setForeground(new Color(102, 102, 255));
+		lblCloseLogin.setFont(new Font("Yu Gothic Medium", Font.BOLD, 18));
+		lblCloseLogin.setBounds(883, 13, 19, 28);
+		panel.add(lblCloseLogin);
+
+		txtUsuario = new JTextField();
+		txtUsuario.setFont(new Font("Yu Gothic UI", Font.PLAIN, 14));
+		txtUsuario.setBounds(294, 234, 314, 36);
+		panel.add(txtUsuario);
+		txtUsuario.setColumns(10);
+
+		txtPassword = new JPasswordField();
+		txtPassword.setFont(new Font("Yu Gothic UI", Font.PLAIN, 14));
+		txtPassword.setBounds(294, 321, 314, 36);
+		panel.add(txtPassword);
+
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setForeground(Color.BLACK);
+		lblUsuario.setFont(new Font("Yu Gothic UI", Font.BOLD, 16));
+		lblUsuario.setBounds(294, 193, 63, 36);
+		panel.add(lblUsuario);
+
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
+		lblContrasea.setForeground(Color.BLACK);
+		lblContrasea.setFont(new Font("Yu Gothic UI", Font.BOLD, 16));
+		lblContrasea.setBounds(294, 281, 91, 36);
+		panel.add(lblContrasea);
+
+		JButton btnNewButton = new JButton("Ingresar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+//				String password = String.valueOf(txtPassword.getPassword());
+//				
+//				if(!(txtUsuario.getText() == "" && password == "")) {
+					Dashboard aux = new Dashboard();
+					aux.setVisible(true);
+					dispose();
+//				}else {
+//				    JOptionPane.showMessageDialog(null, "Revisa los campos para poder ingresar", "Error!",
+//				            JOptionPane.ERROR_MESSAGE);
+//				}
+//				
+			}
+		});
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setBackground(new Color(102, 102, 255));
+		btnNewButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton.setBorder(new LineBorder(new Color(102, 102, 255)));
+		btnNewButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnNewButton.setFont(new Font("Yu Gothic UI", Font.BOLD, 16));
+		btnNewButton.setBounds(294, 386, 314, 36);
+		panel.add(btnNewButton);
+	}
 }

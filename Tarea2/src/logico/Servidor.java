ab61b0068aaf02a13fa1c4cabdfac8734106e10b
kendo <kendo@DESKTOP-QK28CQB>
2021-07-31 22:53:55 -0400

    Cosas básicas agregadas a la clase Servidor

--- Tarea2/src/logico/Servidor.java
+++ Tarea2/src/logico/Servidor.java
@@ -1,5 +1,32 @@
 package logico;
 
-public class Servidor {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+public class Servidor extends Thread {
+	
+	public static Vector entradas = new Vector();
+	
+	public static void main (String args[]) 
+	{
+		
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(7000);
+		} catch (IOException ioe) {
+			System.out.println("Comunicación rechazada."+ioe);
+		    System.exit(1);
+		}
+		
+		while(true) {
+			try {
+				Socket socket = server.accept();
+			} catch (IOException ioe) {
+				System.out.println("Error: "+ioe);
+			}
+		}
+	}
 
 }
